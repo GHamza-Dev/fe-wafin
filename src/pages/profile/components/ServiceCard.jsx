a7f41a80a512c9c00a58a619479dcfa2ba083850
ghamza-dev <elgassaihamzam54@gmail.com>
2022-05-29 17:24:28 +0200

    Improve card style

--- src/pages/profile/components/ServiceCard.jsx
+++ src/pages/profile/components/ServiceCard.jsx
@@ -1,6 +1,10 @@
+import { CgPlayListRemove } from "react-icons/cg";
+import { BiEditAlt } from "react-icons/bi";
+import { RiRadioButtonLine } from "react-icons/ri";
+
 const ServiceCard = ({ image, price, text }) => {
   return (
-    <div className="overflow-hidden shadow-md ring-gray-50 ring-2 rounded-sm">
+    <div className="overflow-hidden shadow-md ring-gray-50  rounded-sm">
       {/* card image */}
       <div className="h-52">
         <img className="h-full w-full object-cover" src={image} alt="roma" />
@@ -11,10 +15,19 @@ const ServiceCard = ({ image, price, text }) => {
         <p className="my-1 line-clamp-3">{text}</p>
       </div>
       {/* card footer */}
-      <div className="flex justify-between p-4">
-        <p className="text-green-400">Active</p>
-        <p className="text-blue">Edit</p>
-        <p className="text-red-400">Suppremer</p>
+      <div className="flex justify-between items-center px-4 py-2 bg-slate-100">
+        <button className="text-green-400 text-sm flex flex-col items-center">
+          <RiRadioButtonLine size={25} />
+          <span>Active</span>
+        </button>
+        <button className="text-blue text-sm flex flex-col items-center">
+          <BiEditAlt size={25} />
+          <span>Edit</span>
+        </button>
+        <button className="text-red-400 text-sm flex flex-col items-center">
+          <CgPlayListRemove size={25} />
+          <span>Supprimer</span>
+        </button>
       </div>
     </div>
   );
